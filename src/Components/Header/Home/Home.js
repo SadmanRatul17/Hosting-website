2c236f8cc3e0117d6466d81be5ddfc43165428c9
SadmanRatul17 <sadmanratul17@gmail.com>
2023-06-12 01:33:49 +0600

    added home

--- src/Components/Header/Home/Home.js
+++ src/Components/Header/Home/Home.js
@@ -1,8 +1,21 @@
 import React from "react";
 import "./Home.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBell } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
+import {
+  faBell,
+  faCloudArrowDown,
+  faCommentSlash,
+  faList,
+  faMessage,
+} from "@fortawesome/free-solid-svg-icons";
+import goggleLogo from "../../Assets/goggle.png";
+import githubLogo from "../../Assets/github.png";
+import windows from "../../Assets/windows.png";
+import android from "../../Assets/android.png";
+import ubuntu from "../../Assets/ubuntu.png";
+import derian from "../../Assets/linux.png";
+import centOs from "../../Assets/centos.png";
+import open from "../../Assets/open.png";
 
 const Home = () => {
   return (
@@ -65,9 +78,94 @@ const Home = () => {
             <p className="text">or sign up with</p>
             <hr className="line2" />
           </div>
-          <div className="button_authentication"></div>
+          <div className="button_authentication">
+            <button className="goggle__button">
+              <img className="goggle__img" src={goggleLogo} alt="" srcset="" />
+              GOOGLE
+            </button>
+            <button className="goggle__button">
+              <img
+                className="goggle__img github__img"
+                src={githubLogo}
+                alt=""
+                srcset=""
+              />
+              GitHub
+            </button>
+          </div>
+          <div className="Last__description">
+            <small>
+              $100, 60-day free trial requires valid credit card. By signing up,
+              you agree to the{" "}
+              <a className="terms" href="#">
+                Terms of Service
+              </a>
+              .
+            </small>
+          </div>
         </div>
       </div>
+      <section className="icon__box home">
+        <div className="box">
+          <FontAwesomeIcon className="box_icon" icon={faMessage} />
+          <p>Anonymous</p>
+          <p>Access</p>
+        </div>
+        <div className="box">
+          <FontAwesomeIcon className="box_icon" icon={faCommentSlash} />
+          <p>No Usage</p>
+          <p>Logs</p>
+        </div>
+        <div className="box">
+          <FontAwesomeIcon className="box_icon" icon={faCloudArrowDown} />
+          <p>No Usage</p>
+          <p>Limits</p>
+        </div>
+        <div className="box">
+          <FontAwesomeIcon className="box_icon" icon={faList} />
+          <p>100% White . .</p>
+          <p>Listed</p>
+        </div>
+      </section>
+
+      <section className="popular-choice">
+        <div>
+          <p className="popular__Text">
+            Popular <br /> operating <br /> systems.
+            <br />
+            <span className="choice">Your Choice.</span>
+          </p>
+          <small className="small-text">
+            With ability to upload your own Operating system.
+          </small>
+        </div>
+        <div className="logo__cards">
+          <div className="card">
+            <img className="card__windows" src={windows} alt="" srcset="" />
+            <p>Windows</p>
+          </div>
+          <div className="card">
+            <img className="card__windows" src={android} alt="" srcset="" />
+            <p>Android</p>
+          </div>
+          <div className="card">
+            <img className="card__windows" src={ubuntu} alt="" srcset="" />
+            <p>Ubuntu</p>
+          </div>
+          <div className="card">
+            <img className="card__windows" src={derian} alt="" srcset="" />
+            <p>Derian</p>
+          </div>
+          <div className="card">
+            <img className="card__windows" src={centOs} alt="" srcset="" />
+            <p>CentOs</p>
+          </div>
+          <div className="card">
+            <img className="card__windows" src={open} alt="" srcset="" />
+            <p>OPen suse</p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
